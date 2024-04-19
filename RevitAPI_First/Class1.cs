489bbacf61754b682843fb9e193d447e1edb0c29
AayushJoshiCCTECH <157628596+AayushJoshiCCTECH@users.noreply.github.com>
2024-04-19 12:41:16 +0530

    Code Updated Successfully

---
+++ RevitAPI_First/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.DB.Architecture;
+namespace RevitAPI_First
+{
+
+    [Transaction(TransactionMode.Manual)]
+    public class Class1 : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            TaskDialog.Show("Hello Aayush..", "Hello Revit API!");
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
